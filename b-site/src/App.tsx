5adae86093821c4c8cf75ac5bf95673bca85f055
BOkeefe <brendan.okeefe@charter.com>
2023-12-08 17:11:07 -0700

    fix main

---
+++ b-site/src/App.tsx
@@ -0,0 +1,17 @@
+
+import { BrowserRouter } from 'react-router-dom'
+import './App.scss'
+import Dash from './components/Dash/Dash.component'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+         <Dash />
+      </BrowserRouter>,
+    </>
+  )
+}
+
+export default App
