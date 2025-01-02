8bf6e9668c8702696a13acf182361c1203787d0e
BrendanMatchPoint <brendan.okeefe@wcgclinical.com>
2025-01-02 13:44:49 -0700

    fix locally

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -7,12 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use('/', express.static(join(__dirname, '..', '..', 'react-fe', 'dist')));
 
-  app.use(
-    '/api',
-    app.use('/api', (req, res, next) => {
-      next();
-    }),
-  );
+  app.use('/api', (req, res, next) => {
+    next();
+  });
 
   await app.listen(4171);
 }
