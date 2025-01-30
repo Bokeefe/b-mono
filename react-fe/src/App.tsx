0b256bc592b8f9d5383f012bb2e47e7580c6f6df
BrendanMatchPoint <brendan.okeefe@wcgclinical.com>
2025-01-30 12:49:48 -0700

    slap

--- react-fe/src/App.tsx
+++ react-fe/src/App.tsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Home from "./components/Home/Home.component";
-import Resume from "./domains/Resume/Resume.component";
 import MainLayout from "./components/MainLayout/MainLayout.component";
-import "./style/variables.css";
+import About from "./domains/About/About.component";
 import AxiosExample from "./domains/Examples/Axios/Axios.component";
 import FetchExample from "./domains/Examples/Fetch/Fetch.component";
-import About from "./domains/About/About.component";
 import Sandbox from "./domains/Examples/Sandbox/Sandbox.component";
+import Resume from "./domains/Resume/Resume.component";
+import "./style/variables.css";
 
 function App() {
   return (
     <>
+      {/* <BrowserRouter> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
