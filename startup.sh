04048dd7aff57fe56170370b7e48f33991792097
Crenshaw Barelywater <crenshawbarelywater@Crenshaws-Air.hsd1.co.comcast.net>
2025-07-14 11:39:54 -0600

    Fix startup script - keep nginx in foreground for Docker

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@
 # Start the backend
 cd /usr/src/app/nest-server && node dist/main.js &
 
-# Start nginx
-nginx -g "daemon off;" &
+# Start nginx in foreground (Docker needs a foreground process)
+nginx -g "daemon off;"
