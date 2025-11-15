a3534a047f1ce4d9365fd9719aae0778db8f7642
Crenshaw Barelywater <crenshawbarelywater@Crenshaws-Air.local>
2025-11-15 12:22:45 -0700

    fix deployment

--- startup.sh
+++ startup.sh
@@ -4,9 +4,9 @@
 cd /usr/src/app/nest-server && node dist/main.js &
 
 # Obtain SSL certificate if not already present
-if [ ! -f /etc/letsencrypt/live/bverse.world/fullchain.pem ]; then
+if [ ! -f /etc/ssl/antigogglin/public.pem ]; then
     echo "Obtaining SSL certificate..."
-    certbot certonly --standalone -d bverse.world -d www.bverse.world --non-interactive --agree-tos --email admin@bverse.world
+    certbot certonly --standalone -d antigogglin.org -d www.antigogglin.org --non-interactive --agree-tos --email admin@antigogglin.org
 fi
 
 # Start nginx in foreground (Docker needs a foreground process)
